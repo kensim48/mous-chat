5231cf89a2d272fb64048411d478534f1d1cf12d
kensim48 <kensim28@hotmail.com>
2021-03-05 14:59:58 +0800

    Updated entire code with NLU GCP integration

---
+++ rasa_chatbot/build_push_gcp.bat
@@ -0,0 +1,2 @@
+docker build . -t gcr.io/carbon-mediator-256404/mouse-chat-rasa-nlu
+docker push gcr.io/carbon-mediator-256404/mouse-chat-rasa-nlu
--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -6,12 +6,14 @@
 """
 Simple Bot to reply to Telegram messages.
 """
+from dotenv import load_dotenv
 
 import logging
 import requests
 import json
 import random
 import string
+import os
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
@@ -23,7 +25,18 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-RASA_CHATBOT_URL = 'http://192.168.1.50:5005/webhooks/rest/webhook'
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+RASA_CHATBOT_URL = os.getenv("RASA_CHATBOT_URL")
+
+DEBUG_MODE = True
+
+list_of_user_intents = {}
+
+list_of_room_intents = {}
+
+pair_details = {}
 
 list_of_pairs = {}
 pair_position = {}
@@ -32,8 +45,7 @@ last_hash = None
 
 tik_tok_chat = True
 
-BOT_CHAT, HUMAN_CHAT = range(2)
-FIRST_QNS, SECOND_QNS, THIRD_QNS = map(chr, range(4))
+BOT_CHAT, HUMAN_CHAT, FIRST_QNS, SECOND_QNS, THIRD_QNS, FOURTH_QNS = range(6)
 
 def random_string():
     return ''.join(random.choice('0123456789ABCDEF') for i in range(16))
@@ -42,33 +54,130 @@ def random_string():
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
+    if DEBUG_MODE:
+        update.message.reply_text('DEBUG MODE ON')
     update.message.reply_text('Hi Mousie_Bot here, good day to you.')
-    
+    update.message.reply_text('How are you today?')
 
     return FIRST_QNS
 
-def firstQns(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('How are you today?')
-    chatbot(update, context)
+def first_qn_function(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    sender_id = update.message.chat.id
+    FIRST_QNS_INTENTS = ['mood_great', 'mood_unhappy', 'mood_soso', 'mood_angry', 'mood_suicidal']
+    intent = determine_intent(get_chatbot_intent(text), FIRST_QNS_INTENTS, update)[0]
+    print('first question', text)
+    list_of_user_intents[sender_id] = [intent]
+    update.message.reply_text('Mousie_Bot wonders what you did today?')
+    
     return SECOND_QNS
+
+def second_qn_function(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    sender_id = update.message.chat.id
+    SECOND_QNS_INTENTS = ['mood_great', 'mood_unhappy', 'mood_soso', 'mood_angry', 'mood_suicidal']
+    intent = determine_intent(get_chatbot_intent(text), SECOND_QNS_INTENTS, update)[0]
+    # intent = determine_intent(get_chatbot_intent(text), FIRST_QNS_INTENTS)
+
+    update.message.reply_text('Mousie_Bot wants to help you to find a friend! Tell me about your hobby~')
+
+    
+    return THIRD_QNS
+
+def third_qn_function(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    sender_id = update.message.chat.id
+
+    THIRD_QUESTION_INTENT = ['hobby_gaming', 'hobby_partying', 'hobby_reading', 'hobby_sports']
+    intent = determine_intent(get_chatbot_intent(text), THIRD_QUESTION_INTENT, update)[0]
+    list_of_user_intents[sender_id].append(intent)
+
+
+    update.message.reply_text('Share Mousie_Bot interesting topics that you want to discuss!')
+    
+    
+    return FOURTH_QNS
+
+def fourth_qn_function(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    sender_id = update.message.chat.id
+
+    FOURTH_QNS_INTENT = ['topic_sustainabilty', 'topic_hobby', 'topic_technology', 'topic_relationship']
+    intent_list = determine_intent(get_chatbot_intent(text), FOURTH_QNS_INTENT, update)
+    list_of_user_intents[sender_id].append(intent_list)
+    
+    update.message.reply_text('Mousie_Bot understands, I will soon match you with another Mousier. Please wait for a moment.')
+    matchmake_user(sender_id)
+    print(sender_id)
+    return HUMAN_CHAT
+
+
+def matchmake_user(sender_id):
+    intent = list_of_user_intents[sender_id]
+    last_hash = None
+    for room_hash in list_of_room_intents:
+        other_intent = list_of_room_intents[room_hash]
+        if (other_intent[0] == "mood_soso" and intent[0] == "mood_soso") or (other_intent[0] == "mood_soso" and intent[0] == "mood_great") or (other_intent[0] == "mood_great" and intent[0] == "mood_soso") or (other_intent[0] == "mood_great" and intent[0] == "mood_great") or (other_intent[0] == "mood_great" and intent[0] == "mood_unhappy") or (other_intent[0] == "mood_unhappy" and intent[0] == "mood_great"):
+            if other_intent[1] == intent[1]:
+                last_hash = room_hash
+            for topics in intent[2]:
+                if topics in other_intent[2]:
+                    last_hash = room_hash
+    if last_hash is None:
+            last_hash = random_string()
+            list_of_pairs[last_hash] = [sender_id]
+            pair_position[sender_id] = last_hash
+            list_of_room_intents[last_hash] = intent
+    else:
+        list_of_pairs[last_hash].append(sender_id)
+        pair_position[sender_id] = last_hash
+        last_hash = None
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
+def get_chatbot_intent(text) -> dict:
+    """get response from chatbot the user message."""
+    payload= json.dumps({"text": text})
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.request("POST", RASA_CHATBOT_URL, headers=headers, data=payload)
+
+    print(response.text)
+    intentList = json.loads(response.text)['intent_ranking']
+
+    # pass back RASA chatbot intent
+    return intentList
+
+def determine_intent(intentList, possibleIntents, update):
+    intent_order = []
+    for intent in intentList:
+        if intent['name'] in possibleIntents:
+            intent_order.append(intent['name'])
+            print(intent_order)
+    if DEBUG_MODE:
+        update.message.reply_text(json.dumps(intent_order))
+    return intent_order
 
 
-def chatbot(update: Update, context: CallbackContext) -> None:
+def displayChatbotResponse(update: Update, context: CallbackContext, reply_list) -> None:
+    for reply in reply_list:
+        if "text" in reply:
+            update.message.reply_text(reply["text"])
+        else:
+            update.message.reply_text(reply["image"])
+    
+def basic_chatbot(update: Update, context: CallbackContext) -> None:
     """chatbot the user message."""
     print(update.message.chat.id)
     payload= json.dumps({"sender": update.message.chat.id,"message": update.message.text})
     headers = {'Content-Type': 'application/json'}
 
-    response = requests.request("POST", RASA_CHATBOT_URL, headers=headers, data=payload)
+    response = requests.request("POST", 'http://192.168.1.50:5005/webhooks/rest/webhook', headers=headers, data=payload)
     print('INPUT: ', update.message.text)
     print(response.text)
     reply_list = json.loads(response.text)
-    context.bot.send_message(chat_id=36160781, text=update.message.text)
     # pass back RASA chatbot response
     for reply in reply_list:
         if "text" in reply:
@@ -80,50 +189,36 @@ def chatbot(update: Update, context: CallbackContext) -> None:
     
 
 def connectUser(update: Update, context: CallbackContext) -> None:
-    global last_hash
     if update.message.chat.id in pair_position:
         actual_pair = list_of_pairs[pair_position[update.message.chat.id]]
-        print(actual_pair)
+        print(list_of_pairs)
         if len(actual_pair) == 2:
             other_user = actual_pair[0] if actual_pair[0]!=update.message.chat.id else actual_pair[1]
             print(list_of_pairs, other_user)
             context.bot.send_message(chat_id=other_user, text=update.message.text)
         else:
             update.message.reply_text("Yo wait")
-    else:
-        print(last_hash)
-        if last_hash is None:
-            last_hash = random_string()
-            list_of_pairs[last_hash] = [update.message.chat.id]
-            pair_position[update.message.chat.id] = last_hash
-            print(last_hash)
-        else:
-            list_of_pairs[last_hash].append(update.message.chat.id)
-            pair_position[update.message.chat.id] = last_hash
-            last_hash = None
 
 
 
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1652772350:AAH4J5QW9-aZZWZT2Qpf0OtcQRvXtVGcAGo")
+    updater = Updater(TELEGRAM_TOKEN)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
     conv_handler = ConversationHandler(
-        # entry_points=[MessageHandler(Filters.text & ~Filters.command, connectUser)],
+        # entry_points=[MessageHandler(Filters.text & ~Filters.command, fourth_qn_function)],
         entry_points=[CommandHandler('start', start)],
         states={
-            FIRST_QNS: [ MessageHandler(Filters.text & ~Filters.command, firstQns)],
-            BOT_CHAT: [
-                MessageHandler(Filters.text & ~Filters.command, chatbot)
-            ],
-            HUMAN_CHAT: [
-                MessageHandler(Filters.text & ~Filters.command, connectUser)
-            ]
+            FIRST_QNS: [ MessageHandler(Filters.text & ~Filters.command, first_qn_function)],
+            SECOND_QNS: [ MessageHandler(Filters.text & ~Filters.command, second_qn_function)],
+            THIRD_QNS: [ MessageHandler(Filters.text & ~Filters.command, third_qn_function)],
+            FOURTH_QNS: [ MessageHandler(Filters.text & ~Filters.command, fourth_qn_function)],
+            HUMAN_CHAT: [ MessageHandler(Filters.text & ~Filters.command, connectUser)]
         },
         fallbacks=[MessageHandler(Filters.regex('start'), start)],
     )
